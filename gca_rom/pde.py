bd19e319eaf3692bf645add5199a423726b39145
Carlotta Filippin <120277978+Crltt@users.noreply.github.com>
2023-11-15 11:28:46 +0100

    Update pde.py

--- gca_rom/pde.py
+++ gca_rom/pde.py
@@ -98,21 +98,21 @@ def problem(argument):
         case 11:
             problem_name = "maxwell"
             variable = 'TEz'
-            mu1 = np.linspace(1., 1., 326)
+            mu1 = np.linspace(0., 325., 1)
             mu2 = np.linspace(1., 1.)
             mu_space = [mu1, mu2]
             n_param = 2
         case 12:
             problem_name = "maxwell"
             variable = 'THx'
-            mu1 = np.linspace(0., 1., 326)
+            mu1 = np.linspace(0., 325., 1)
             mu2 = np.linspace(1., 1.)
             mu_space = [mu1, mu2]
             n_param = 2
         case 13:
             problem_name = "maxwell"
             variable = 'THy'
-            mu1 = np.linspace(0., 1., 326)
+            mu1 = np.linspace(0., 325., 1)
             mu2 = np.linspace(1., 1.)
             mu_space = [mu1, mu2]
             n_param = 2
