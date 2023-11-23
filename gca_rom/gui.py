376991a849f8dc1e12853a4a2fcc6d321015d1ec
Carlotta Filippin <120277978+Crltt@users.noreply.github.com>
2023-11-23 16:01:26 +0100

    Update gui.py

--- gca_rom/gui.py
+++ gca_rom/gui.py
@@ -50,14 +50,14 @@ def hyperparameters_selection(name, var, n):
         elif name == "stokes_u":
             preset = [3, 2, 2, 2, 1, 3, 3, 1, 0]
         elif name == "maxwell":
-            preset = [3, 2, 2, 2, 1, 1, 1, 1, 1]
+            preset = [3, 2, 3, 2, 1, 1, 1, 1, 1]
         return preset
 
 
     preset = default_values(name)
     preset_options_1 = ["sample", "feature", "feature-sampling", "sampling-feature"] 
     preset_options_2 = ["minmax", "robust", "standard"]
-    preset_options_3 = ["10", "20", "30", "40", "70"]
+    preset_options_3 = ["10", "20", "30", "40", "50"]
     preset_options_4 = ["50", "100", "200", "300", "400"]
     preset_options_5 = ["25", "50", "75", "100", "125"]
     preset_options_6 = ["10", "15", "20", "25", "30"]
