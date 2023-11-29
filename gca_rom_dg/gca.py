b5831bfd04e9db8191961a488f55b99c50d27853
Carlotta Filippin <120277978+Crltt@users.noreply.github.com>
2023-11-29 17:58:51 +0100

    Update gca.py

--- gca_rom_dg/gca.py
+++ gca_rom_dg/gca.py
@@ -2,7 +2,7 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 #from torch_geometric.nn import GMMConv
-from gca_rom_dg import con
+from gca_rom_dg import conv
 from conv import GMM
 
 class Encoder(torch.nn.Module):
