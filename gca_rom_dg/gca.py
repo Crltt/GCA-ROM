627d63f45ba1959640128506cafcf2063ebe0193
Carlotta Filippin <120277978+Crltt@users.noreply.github.com>
2023-12-18 10:20:16 +0100

    Update gca.py

--- gca_rom_dg/gca.py
+++ gca_rom_dg/gca.py
@@ -40,7 +40,7 @@ class Encoder(torch.nn.Module):
         self.down_convs = torch.nn.ModuleList()
         for i in range(self.depth-1):
             print('hidden_channels: ',hidden_channels[i])
-            self.down_convs.append(GMMConv(self.hidden_channels[i], self.hidden_channels[i+1], dim=1, kernel_size=1, aggr='mean')) 
+            self.down_convs.append(GMMConv(self.hidden_channels[i], self.hidden_channels[i+1], dim=1, kernel_size=5, aggr='mean')) 
 
         print('input_size: ', self.input_size)
         print('ffn: ', self.ffn)
@@ -121,7 +121,7 @@ class Decoder(torch.nn.Module):
 
         self.up_convs = torch.nn.ModuleList()
         for i in range(self.depth-1):
-            self.up_convs.append(GMMConv(self.hidden_channels[self.depth-1-i], self.hidden_channels[self.depth-i-2], dim=1, kernel_size=1, aggr='mean')) 
+            self.up_convs.append(GMMConv(self.hidden_channels[self.depth-1-i], self.hidden_channels[self.depth-i-2], dim=1, kernel_size=5, aggr='mean')) 
         
         self.reset_parameters()
 
