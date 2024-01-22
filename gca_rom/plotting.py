e2b88e710eb364f886393e401f1314f3d89748fb
Carlotta Filippin <120277978+Crltt@users.noreply.github.com>
2024-01-22 10:38:26 +0100

    Update plotting.py

--- gca_rom/plotting.py
+++ gca_rom/plotting.py
@@ -34,7 +34,7 @@ def plot_loss(HyperParams):
     plt.ylabel('Loss')
     plt.ylabel('Epochs')
     plt.title('Loss over training epochs')
-    plt.legend(['loss_mse', 'loss_map', 'loss_test_mse', 'loss_test_map'])
+    plt.legend(['Autoencoder (train)', 'Map (train)', 'Autoencoder (test)', 'Map (test)'])
     plt.savefig(HyperParams.net_dir+'history_losses'+HyperParams.net_run+'.png', bbox_inches='tight', dpi=500)
 
 
@@ -48,17 +48,22 @@ def plot_latent(HyperParams, latents, latents_estimation):
     latents_estimation (tensor): tensor of estimated latent spaces
     """
 
-    plt.figure()
+     plt.figure()
     for i1 in range(HyperParams.bottleneck_dim):
         plt.plot(latents[:,i1].detach(), '--')
         plt.plot(latents_estimation[:,i1].detach(),'-')
-    plt.savefig(HyperParams.net_dir+'latents'+HyperParams.net_run+'.png', bbox_inches='tight')
-    
+    plt.title('Evolution in the latent space')
+    plt.ylabel('$u_N(\mu)$')
+    plt.xlabel('Snaphots')
+    plt.legend(['Autoencoder', 'Map'])
+    plt.savefig(HyperParams.net_dir+'latents'+HyperParams.net_run+'.png', bbox_inches='tight', dpi=500)
     green_diamond = dict(markerfacecolor='g', marker='D')
     _, ax = plt.subplots()
     ax.boxplot(latents_estimation.detach().numpy(), flierprops=green_diamond)
+    plt.title('Variance in the latent space')
+    plt.ylabel('$u_N(\mu)$')
+    plt.xlabel('Bottleneck')
     plt.savefig(HyperParams.net_dir+'box_plot_latents'+HyperParams.net_run+'.png', bbox_inches='tight', dpi=500)
-    
 
 def plot_error(res, VAR_all, scaler_all, HyperParams, mu_space, params, train_trajectories, vars, p1=0, p2=-1):
     """
