00429c2ee1cc8e6a7741a08efa49e26791dee7fa
Carlotta Filippin <120277978+Crltt@users.noreply.github.com>
2024-01-24 14:19:56 +0100

    Add files via upload

--- gca_rom/plotting.py
+++ gca_rom/plotting.py
@@ -224,3 +224,61 @@ def plot_error_fields(SNAP, results, VAR_all, scaler_all, HyperParams, dataset, 
     ax.set_aspect('equal', 'box')
     ax.set_title('Error field for $\mu$ = '+str(np.around(params[SNAP].detach().numpy(), 2)))
     plt.savefig(HyperParams.net_dir+'error_field_'+str(SNAP)+''+HyperParams.net_run+'.png', bbox_inches='tight', dpi=500)
+
+def plot_analytic_error_fields(SNAP, results, VAR_all, scaler_all, HyperParams, dataset, xyz, params):
+    """
+    This function plots a contour map of the error field of a given solution of a scalar field.
+    The error is computed as the absolute difference between the true solution and the predicted solution,
+    normalized by the 2-norm of the true solution.
+
+    Inputs:
+    SNAP: int, snapshot of the solution to be plotted
+    results: np.array, predicted solution
+    VAR_all: np.array, true solution
+    scaler_all: np.array, scaling information used in the prediction
+    HyperParams: class, model architecture and training parameters
+    dataset: np.array, mesh information
+    xyz: list of arrays of shape (num_samples, num_features), containing the x, y and z-coordinates of the domain.
+    params: np.array, model parameters
+    """
+
+    fig = plt.figure()
+    Z = scaling.inverse_scaling(VAR_all, scaler_all, HyperParams.scaling_type)
+    Z_net = scaling.inverse_scaling(results, scaler_all, HyperParams.scaling_type)
+    z = Z[:, SNAP]
+    z_net = Z_net[:, SNAP]
+    #error = abs(z - z_net)/np.linalg.norm(z, 2)
+
+    [error, norm_a]=error.compute_analytic_error(results,dataset, scaler_all, HyperParams)
+    xx = xyz[0]
+    yy = xyz[1]
+    if dataset.dim == 2:
+        triang = np.asarray(dataset.T - 1)
+        cmap = cm.get_cmap(name='jet', lut=None) 
+        gs1 = gridspec.GridSpec(1, 1)
+        ax = plt.subplot(gs1[0, 0])   
+        cs = ax.tricontourf(xx[:, SNAP], yy[:, SNAP], triang, error, 100, cmap=cmap)
+        divider = make_axes_locatable(ax)
+        cax = divider.append_axes("right", size="5%", pad=0.1)
+        cbar = plt.colorbar(cs, cax=cax)
+    elif dataset.dim == 3:
+        zz = xyz[2]
+        ax = fig.add_subplot(projection='3d')
+        cax = inset_axes(ax, width="5%", height="60%", loc="center left", 
+                         bbox_to_anchor=(1.15, 0., 1, 1), bbox_transform=ax.transAxes, borderpad=0)
+        cmap = cm.get_cmap(name='jet', lut=None) 
+        p = ax.scatter(xx[:, SNAP], yy[:, SNAP], zz[:, SNAP], c=error, cmap=cmap, linewidth=0.5)
+        cbar = fig.colorbar(p, ax=ax, cax=cax)
+        ax.set_xlabel('$x$')
+        ax.set_ylabel('$y$')
+        ax.set_zlabel('$z$')
+        ax.locator_params(axis='both', nbins=5)
+    tick_locator = MaxNLocator(nbins=5)
+    cbar.locator = tick_locator
+    cbar.formatter.set_powerlimits((0, 0))
+    cbar.update_ticks()
+    plt.tight_layout()
+    ax.set_aspect('equal', 'box')
+    ax.set_title('Error field for $\mu$ = '+str(np.around(params[SNAP].detach().numpy(), 2)))
+    plt.savefig(HyperParams.net_dir+'error_field_'+str(SNAP)+''+HyperParams.net_run+'.png', bbox_inches='tight', dpi=500)
+ 
